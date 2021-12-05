ad6c5cffd24fcdcdf472d0025e51d19d92c81807
Yuvraj-R <73365222+Yuvraj-R@users.noreply.github.com>
2021-12-05 12:16:30 -0500

    Add files via upload

---
+++ teamcode/auto/BlueLowerAuto.java
@@ -0,0 +1,315 @@
+package org.firstinspires.ftc.teamcode.auto;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.Point;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+import org.openftc.easyopencv.OpenCvPipeline;
+
+@Autonomous
+public class BlueLowerAuto extends LinearOpMode
+{
+    public enum ElementPosition
+    {
+        LEFT,
+        CENTER,
+        RIGHT
+    }
+
+    OpenCvInternalCamera Cam;
+    SkystoneDeterminationPipeline pipeline;
+
+    public DcMotorEx FL;
+    public DcMotorEx FR;
+    public DcMotorEx BL;
+    public DcMotorEx BR;
+    public DcMotorEx lift;
+    public CRServo lift_extender;
+    public CRServo lift_open;
+    public CRServo rightspinner;
+    public CRServo leftspinner;
+    public CRServo rightextend;
+    public CRServo leftextend;
+    public ElementPosition pos;
+
+    @Override
+    public void runOpMode()
+    {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+        Cam = OpenCvCameraFactory.getInstance().createInternalCamera(OpenCvInternalCamera.CameraDirection.BACK, cameraMonitorViewId);
+        pipeline = new SkystoneDeterminationPipeline();
+        Cam.setPipeline(pipeline);
+        Cam.setViewportRenderingPolicy(OpenCvCamera.ViewportRenderingPolicy.OPTIMIZE_VIEW);
+
+        Cam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener()
+        {
+            @Override
+            public void onOpened()
+            {
+                Cam.startStreaming(320,240, OpenCvCameraRotation.SIDEWAYS_LEFT);
+            }
+
+            @Override
+            public void onError(int errorCode){}
+        });
+
+        BR = hardwareMap.get(DcMotorEx.class, "backright");
+        BL = hardwareMap.get(DcMotorEx.class, "backleft");
+        FR = hardwareMap.get(DcMotorEx.class, "frontright");
+        FL = hardwareMap.get(DcMotorEx.class, "frontleft");
+        lift = hardwareMap.get(DcMotorEx.class, "slides");
+        lift_extender = hardwareMap.get(CRServo.class, "bucketextend");
+        lift_open = hardwareMap.get(CRServo.class, "bucketdrop");
+        rightspinner = hardwareMap.get(CRServo.class, "rightspinner");
+        leftspinner = hardwareMap.get(CRServo.class, "leftspinner");
+        rightextend = hardwareMap.get(CRServo.class, "rightextend");
+        leftextend = hardwareMap.get(CRServo.class, "leftextend");
+
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FL.setDirection(DcMotor.Direction.REVERSE);
+        BL.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        for (int i = 0; i < 10; i++);
+        {
+            pos = pipeline.getAnalysis();
+            sleep(100);
+        }
+
+        lift_extender.setPower(0.5);
+
+        move(-600);
+        runtopos();
+        power(0.25);
+        while(FL.isBusy()){}
+        power(0);
+        reset();
+
+        turn(550);
+        runtopos();
+        power(0.25);
+        while(FL.isBusy()){}
+        power(0);
+        reset();
+
+        move(-910);
+        runtopos();
+        power(0.15);
+        while(FL.isBusy()){}
+        power(0);
+        reset();
+
+        turn(-550);
+        runtopos();
+        power(0.25);
+        while(FL.isBusy()){}
+        power(0);
+        reset();
+
+        if (pos == ElementPosition.LEFT) {
+            rise(0);
+        }else if (pos == ElementPosition.CENTER){
+            rise(1000);
+        }else{
+            rise(2000);
+        }
+        lift.setVelocity(30);
+        drop();
+        fall();
+
+        lift_extender.setPower(0.5);
+
+        sleep(500);
+
+        turn(550);
+        runtopos();
+        power(0.25);
+        while(FL.isBusy()){}
+        power(0);
+        reset();
+
+        move(3000);
+        runtopos();
+        power(0.25);
+        while(FL.isBusy()){}
+        power(0);
+        reset();
+
+    }
+    //Forward towards intake
+    public void move(int ticks)
+    {
+        FL.setTargetPosition(ticks);
+        FR.setTargetPosition(ticks);
+        BL.setTargetPosition(ticks);
+        BR.setTargetPosition(ticks);
+    }
+
+    //Toward right facing intake
+    public void turn(int ticks)
+    {
+        FL.setTargetPosition(ticks);
+        FR.setTargetPosition(-ticks);
+        BL.setTargetPosition(ticks);
+        BR.setTargetPosition(-ticks);
+    }
+
+    public void power(double power)
+    {
+        FL.setPower(power);
+        FR.setPower(power);
+        BL.setPower(power);
+        BR.setPower(power);
+    }
+
+    public void reset()
+    {
+        FL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        FR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        BR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+    }
+
+    public void runtopos()
+    {
+        FL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        FR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        BR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+    }
+
+    public void rise(int time){
+        lift.setVelocity(600);
+        sleep(time);
+        lift.setVelocity(0);
+    }
+
+    public void fall(){
+        lift.setVelocity(0);
+        sleep(1500);
+    }
+
+    public void drop() {
+        lift_extender.setPower(-0.5);
+        sleep(500);
+        lift_open.setPower(-0.6);
+        sleep(1000);
+        lift_extender.setPower(0);
+        sleep(500);
+        lift_open.setPower(0);
+        sleep(500);
+    }
+
+    public static class SkystoneDeterminationPipeline extends OpenCvPipeline
+    {
+        static final Scalar RED = new Scalar(255, 0, 0);
+        static final Point REGION1_TOPLEFT_ANCHOR_POINT = new Point(100,160);
+        static final Point REGION2_TOPLEFT_ANCHOR_POINT = new Point(270,160);
+        static final int REGION_WIDTH = 20;
+        static final int REGION_HEIGHT = 20;
+
+        Point region1_pointA = new Point(REGION1_TOPLEFT_ANCHOR_POINT.x, REGION1_TOPLEFT_ANCHOR_POINT.y);
+        Point region1_pointB = new Point(REGION1_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH, REGION1_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+        Point region2_pointA = new Point(REGION2_TOPLEFT_ANCHOR_POINT.x, REGION2_TOPLEFT_ANCHOR_POINT.y);
+        Point region2_pointB = new Point(REGION2_TOPLEFT_ANCHOR_POINT.x + REGION_WIDTH, REGION2_TOPLEFT_ANCHOR_POINT.y + REGION_HEIGHT);
+
+        Mat region1_Cb, region2_Cb;
+        Mat YCrCb = new Mat();
+        Mat Cb = new Mat();
+        int avg1, avg2;
+        int min;
+        private volatile ElementPosition position = ElementPosition.LEFT;
+
+        void inputToCb(Mat input)
+        {
+            Imgproc.cvtColor(input, YCrCb, Imgproc.COLOR_RGB2YCrCb);
+            Core.extractChannel(YCrCb, Cb, 1);
+        }
+
+        @Override
+        public void init(Mat firstFrame)
+        {
+            inputToCb(firstFrame);
+
+            region1_Cb = Cb.submat(new Rect(region1_pointA, region1_pointB));
+            region2_Cb = Cb.submat(new Rect(region2_pointA, region2_pointB));
+        }
+
+        @Override
+        public Mat processFrame(Mat input)
+        {
+            inputToCb(input);
+
+            avg1 = (int) Core.mean(region1_Cb).val[0];
+            avg2 = (int) Core.mean(region2_Cb).val[0];
+
+            Imgproc.rectangle(
+                    input,
+                    region1_pointA,
+                    region1_pointB,
+                    RED,
+                    2);
+            Imgproc.rectangle(
+                    input,
+                    region2_pointA,
+                    region2_pointB,
+                    RED,
+                    2);
+
+
+            if (avg1 <= 110)
+            {
+                min = 1;
+            }
+            else if (avg2 <= 110)
+            {
+                min = 2;
+            }
+            else
+            {
+                min = 3;
+            }
+
+            if(min == 1)
+            {
+                position = ElementPosition.CENTER;
+            }
+            else if(min == 2)
+            {
+                position = ElementPosition.RIGHT;
+            }
+            else if(min == 3)
+            {
+                position = ElementPosition.LEFT;
+            }
+
+            return input;
+        }
+
+        public ElementPosition getAnalysis()
+        {
+            return position;
+        }
+        public int[] getAvgs()
+        {
+            int[] avgs= {avg1, avg2};
+            return avgs;
+        }
+    }
+}
---
+++ teamcode/drive/TeleOp.java
@@ -0,0 +1,154 @@
+package org.firstinspires.ftc.teamcode.drive;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@com.qualcomm.robotcore.eventloop.opmode.TeleOp
+public class TeleOp extends LinearOpMode {
+
+    public DcMotor FRONT_LEFT;
+    public DcMotor FRONT_RIGHT;
+    public DcMotor BACK_LEFT;
+    public DcMotor BACK_RIGHT;
+
+    public DcMotorEx lift;
+
+    public DcMotor intakeMotor;
+
+    // Servos
+    public Servo dropdown;
+
+    public CRServo lift_extender;
+    public CRServo lift_open;
+
+    public CRServo rightExtend;
+    public CRServo rightSpinner;
+    public CRServo leftExtend;
+    public CRServo leftSpinner;
+
+    public Servo cone;
+
+    // Other
+    public boolean isDropExtended = false;
+
+    public void runOpMode(){
+
+        // Attach hardware variables to software variables
+        FRONT_LEFT = hardwareMap.get(DcMotor.class, "frontleft");
+        FRONT_RIGHT = hardwareMap.get(DcMotor.class, "frontright");
+        BACK_LEFT = hardwareMap.get(DcMotor.class, "backleft");
+        BACK_RIGHT = hardwareMap.get(DcMotor.class, "backright");
+
+        lift = hardwareMap.get(DcMotorEx.class, "slides");
+
+        intakeMotor = hardwareMap.get(DcMotor.class, "intakemotor");
+
+        dropdown = hardwareMap.get(Servo.class, "intakeramp");
+
+        lift_extender = hardwareMap.get(CRServo.class, "bucketextend");
+        lift_open = hardwareMap.get(CRServo.class, "bucketdrop");
+
+        rightExtend = hardwareMap.get(CRServo.class, "rightextend");
+        rightSpinner = hardwareMap.get(CRServo.class, "rightspinner");
+        leftExtend = hardwareMap.get(CRServo.class, "leftextend");
+        leftSpinner = hardwareMap.get(CRServo.class, "leftspinner");
+
+        cone = hardwareMap.get(Servo.class, "cone");
+
+
+        // Set motor direction
+        FRONT_LEFT.setDirection(DcMotorSimple.Direction.REVERSE);
+        BACK_LEFT.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        // Lift encoder
+        leftSpinner.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        waitForStart();
+
+        if(opModeIsActive()){
+
+            while(opModeIsActive()){
+                telemetry.addData("Is Intake Extended?", isDropExtended);
+                telemetry.update();
+
+                // LOOP CODE HERE
+
+                // Gamepad 1
+
+                //Drivetrain
+                if (gamepad1.left_trigger == 0)
+                {
+                    FRONT_LEFT.setPower(-gamepad1.left_stick_y);
+                    BACK_LEFT.setPower(-gamepad1.left_stick_y);
+
+                    FRONT_RIGHT.setPower(-gamepad1.right_stick_y);
+                    BACK_RIGHT.setPower(-gamepad1.right_stick_y);
+                }
+                else
+                {
+                    FRONT_LEFT.setPower(gamepad1.left_stick_y);
+                    BACK_LEFT.setPower(gamepad1.left_stick_y);
+
+                    FRONT_RIGHT.setPower(gamepad1.right_stick_y);
+                    BACK_RIGHT.setPower(gamepad1.right_stick_y);
+                }
+
+
+                // Carousel spinners
+                if (gamepad1.right_bumper){
+                    rightExtend.setPower(0.75);
+                    leftExtend.setPower(0.15);
+                }else{
+                    rightExtend.setPower(0.3);
+                    leftExtend.setPower(0.6);
+                }
+                rightSpinner.setPower(0.7 * gamepad1.right_trigger);
+                leftSpinner.setPower(0.7 * gamepad1.right_trigger);
+
+                //Gamepad 2
+
+                // intake
+                intakeMotor.setPower(-gamepad2.left_stick_y);
+
+                if (gamepad2.x)
+                {
+                    isDropExtended = !isDropExtended;
+                    sleep(200);
+                }
+                if (isDropExtended)
+                {
+                    dropdown.setPosition(2);
+                }
+                else
+                {
+                    dropdown.setPosition(0.2);
+                }
+
+                // lift
+
+                // Option 1
+                if (-gamepad2.right_stick_y >= 0.01){
+                    lift.setPower(-0.7 * gamepad2.right_stick_y);
+                }
+                if (-gamepad2.right_stick_y <= -0.01){
+                    lift.setPower(-0.25 * gamepad2.right_stick_y);
+                }
+
+                // lift open
+                if (-gamepad2.right_trigger != 0){
+                    lift_open.setPower(-0.6);
+                }else{
+                    lift_open.setPower(0);
+                }
+
+                // lift entender
+                lift_extender.setPower(-gamepad2.left_trigger);
+            }
+        }
+
+    }
+}
